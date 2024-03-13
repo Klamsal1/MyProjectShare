20c3e14adeb4f642529db107ea3fdaf243ab6c8e
Kedar, Lamsal <kedarl@gmail.com>
2024-03-12 19:44:37 -0500

    second commits here

---
+++ src/main/java/Book.java
@@ -0,0 +1,13 @@
+public class Book {
+
+     public int sum ( int num1, int num2){
+
+         return num1 + num2;
+     }
+
+    public static void main(String[] args) {
+         Book b = new Book();
+        System.out.println( b.sum(9,8));
+
+    }
+}
